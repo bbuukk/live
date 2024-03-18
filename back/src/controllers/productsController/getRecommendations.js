c2c0d6a2bf19895cd6b912da862f529db24531a3
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-03-18 09:56:01 +0200

    Drop this commit

--- back/src/controllers/productsController/getRecommendations.js
+++ back/src/controllers/productsController/getRecommendations.js
@@ -7,6 +7,7 @@ export const getRecommendations = async (req, res) => {
   const { id } = req.params;
   try {
     const product = await Product.findById(id).populate("category");
+    //all products we have
     const products = await Product.find({}).select("description").exec();
 
     return res.status(200).json({ product, products });
