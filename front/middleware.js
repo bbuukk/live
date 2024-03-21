24f067c6a2cfc0dc646fb29dc9babcb1ed04eff4
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-03-21 06:47:57 +0200

    Profile Image on production fix
    - Trying to change styles on profile image as on production it behaves diffrently that localy

--- front/middleware.js
+++ front/middleware.js
@@ -1,7 +1,3 @@
-// export { default } from "next-auth/middleware";
-
-// export const config = { matcher: ["/profile/:path*"] };
-
 import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
