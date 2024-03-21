7a8369ecf50172dd718afae8c8b119fa7398f45b
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-03-21 06:31:48 +0200

    Review Modal 1.0.0

--- front/features/products/landing/comps/reviews/reviews-list.js
+++ front/features/products/landing/comps/reviews/reviews-list.js
@@ -1,51 +1,51 @@
-import Image from "next/image";
-import { useState } from "react";
-// import ReviewCard from "comps/review-card";
-import s from "./reviews-list.module.scss";
-import WriteReviewModal from "./write-review-modal";
-const ReviewsList = () => {
-  const [showWriteReviewModal, setShowWriteReviewModal] = useState(false);
+// import Image from "next/image";
+// import { useState } from "react";
+// // import ReviewCard from "comps/review-card";
+// import s from "./reviews-list.module.scss";
 
-  return (
-    <>
-      <div className={`${s.reviews}`}>
-        <div className={`${s.header}`}>
-          <p>Відгуки покупців:</p>
-          <button
-            onClick={() => {
-              setShowWriteReviewModal(!showWriteReviewModal);
-            }}
-            className={`${s.write_rev_btn} btn`}
-          >
-            <div className="icon-link">
-              <p>Написати відгук </p>
-              <i className="bi bi-pencil-fill"></i>
-            </div>
-            <Image
-              className={`${s.wave}`}
-              src="/wave.svg"
-              width={100}
-              height={100}
-            ></Image>
-          </button>
-        </div>
-        <div className={`${s.reviews_list}`}>
-          {/* <ReviewCard />
-          <ReviewCard />
-          <ReviewCard /> */}
-          <button className={`${s.all_reviews_btn} btn`}>
-            Дивитись усі відгуки по товару <i className="bi bi-arrow-right"></i>
-          </button>
-        </div>
-      </div>
-      <WriteReviewModal
-        isOpen={showWriteReviewModal}
-        toggle={() => {
-          setShowWriteReviewModal(!showWriteReviewModal);
-        }}
-      />
-    </>
-  );
-};
+// const ReviewsList = () => {
+//   const [showWriteReviewModal, setShowWriteReviewModal] = useState(false);
 
-export default ReviewsList;
+//   return (
+//     <>
+//       <div className={`${s.reviews}`}>
+//         <div className={`${s.header}`}>
+//           <p>Відгуки покупців:</p>
+//           <button
+//             onClick={() => {
+//               setShowWriteReviewModal(!showWriteReviewModal);
+//             }}
+//             className={`${s.write_rev_btn} btn`}
+//           >
+//             <div className="icon-link">
+//               <p>Написати відгук </p>
+//               <i className="bi bi-pencil-fill"></i>
+//             </div>
+//             <Image
+//               className={`${s.wave}`}
+//               src="/wave.svg"
+//               width={100}
+//               height={100}
+//             ></Image>
+//           </button>
+//         </div>
+//         <div className={`${s.reviews_list}`}>
+//           {/* <ReviewCard />
+//           <ReviewCard />
+//           <ReviewCard /> */}
+//           <button className={`${s.all_reviews_btn} btn`}>
+//             Дивитись усі відгуки по товару <i className="bi bi-arrow-right"></i>
+//           </button>
+//         </div>
+//       </div>
+//       <WriteReviewModal
+//         isOpen={showWriteReviewModal}
+//         toggle={() => {
+//           setShowWriteReviewModal(!showWriteReviewModal);
+//         }}
+//       />
+//     </>
+//   );
+// };
+
+// export default ReviewsList;
